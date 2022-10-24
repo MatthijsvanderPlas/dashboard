72f80d30429ff8432130ab9b59ff9035c7eed835
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-10-24 08:06:38 +0200

    Basic Chart working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-//docs.google.com/spreadsheets/d/1BHjq5MjpuSItvVbnQcEdQt_v956-Ks1lr3f_nEFkTks/edit?usp=sharing
 import { useEffect, useState } from 'react';
 import { BarChart } from './components/BarChart';
 
@@ -14,6 +13,8 @@ function App() {
   const [labels, setLabels] = useState<string[]>([]);
   const [csvDifficulty, setCsvDifficulty] = useState<number[]>([]);
   const [csvFun, setCsvFun] = useState<number[]>([]);
+  const [slice, setSlice] = useState<number[]>([0, 10]);
+  const [total] = useState<number>(56);
 
   const sheetId = '1BHjq5MjpuSItvVbnQcEdQt_v956-Ks1lr3f_nEFkTks';
   const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
@@ -40,32 +41,54 @@ function App() {
   }, [url]);
 
   useEffect(() => {
-    let LabelsArray = Array.from(parsedCsvData, (item) => item.assignment);
-    const LabelsSet = new Set(LabelsArray);
+    let LabelsArray: string[] = Array.from(parsedCsvData, (item) => item.assignment);
+    const LabelsSet: Set<string> = new Set(LabelsArray);
     LabelsArray = [...LabelsSet];
-    setLabels([...LabelsArray]);
+    setLabels([...LabelsArray.slice(...slice)]);
 
-    const difficultyArray = [];
-
-    for (let i = 0; i < LabelsArray.length; i++) {
-      const number = parsedCsvData.filter((item) => item.assignment === LabelsArray[i]);
-      difficultyArray.push(number.reduce((a, b) => a + b.difficulty, 0) / number.length);
-    }
-    setCsvDifficulty([...difficultyArray]);
-
-    const funArray = [];
+    const difficultyArray: number[] = [];
 
     for (let i = 0; i < LabelsArray.length; i++) {
       const number = parsedCsvData.filter((item) => item.assignment === LabelsArray[i]);
-      funArray.push(number.reduce((a, b) => a + b.fun, 0) / number.length);
+      difficultyArray.push(number.reduce((a, b) => a + b.difficulty, 0) / number.length) as number;
     }
-    setCsvFun([...funArray]);
-  }, [parsedCsvData]);
+    setCsvDifficulty([...difficultyArray.slice(...slice)]);
+
+    const funArray: number[] = [];
+
+    for (let i = 0; i < LabelsArray.length; i++) {
+      const number = parsedCsvData.filter((item) => item.assignment === LabelsArray[i]);
+      funArray.push(number.reduce((a, b) => a + b.fun, 0) / number.length) as number;
+    }
+    setCsvFun([...funArray.slice(...slice)]);
+  }, [parsedCsvData, slice]);
+
+  const nextSlice = () => {
+    if (slice[1] >= total) return;
+    setSlice((prev) => [prev[1], prev[1] + 10]);
+  };
+
+  const prevSlice = () => {
+    if (slice[0] === 0) return;
+    setSlice((prev) => [prev[0] - 10, prev[0]]);
+  };
 
   return (
-    <>
+    <div className='mx-auto max-w-5xl my-8'>
       <BarChart labels={labels} csvdifficulty={csvDifficulty} csvfun={csvFun} />
-    </>
+      <button
+        className='mx-4 px-5 mt-8 border-purple-300 border-2 rounded active:scale-90 shadow-gray-500 shadow-md'
+        onClick={prevSlice}
+      >
+        Previous Slice
+      </button>
+      <button
+        className='mx-4 px-5 border-sky-300 border-2 rounded active:scale-90 shadow-gray-500 shadow-md'
+        onClick={nextSlice}
+      >
+        Next Slice
+      </button>
+    </div>
   );
 }
 
