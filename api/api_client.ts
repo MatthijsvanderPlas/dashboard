dd31f48ea07183cee6307dd5184173ff84865b5f
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-10-24 22:01:53 +0200

    Migrating to Redux

---
+++ api/api_client.ts
@@ -0,0 +1,25 @@
+import { CsvData } from 'store/studentDataSlice';
+
+const sheetId = '1BHjq5MjpuSItvVbnQcEdQt_v956-Ks1lr3f_nEFkTks';
+const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+const sheetName = 'Winc Eindopdracht – Studenten Mock data – Frontend Cursus Opdrachten';
+const query = encodeURIComponent('Select *');
+const url = `${base}&sheet=${sheetName}&tq=${query}`;
+
+const fetchData = async () => {
+  const response = await fetch(url);
+  const rep = await response.text();
+  const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
+  const data: CsvData[] = [];
+  jsonData.table.rows.forEach((item) => {
+    data.push({
+      name: item.c[0].v,
+      assignment: item.c[1].v,
+      difficulty: item.c[2].v,
+      fun: item.c[3].v,
+    });
+  });
+  return data;
+};
+
+export default fetchData;
