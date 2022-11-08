fdc219d02aa2df3c99ac3171adb11605ae619134
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-11-08 23:21:41 +0100

    fiddling with chart libraries

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -4,63 +4,62 @@ const Example = () => {
   const data = [
     {
       day: 'Monday',
-      degress: 59,
+      degrees: 59,
     },
     {
       day: 'Tuesday',
-      degress: 61,
+      degrees: 61,
     },
     {
       day: 'Wednesday',
-      degress: 55,
+      degrees: 55,
     },
     {
       day: 'Thursday',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'Thurs',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'Thury',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'ay',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'y',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'day',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'Thursday',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'Thursday',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'Thursday',
-      degress: 78,
+      degrees: 78,
     },
     {
       day: 'Thursday',
-      degress: 78,
+      degrees: 78,
     },
   ];
 
   return (
     <ResponsiveBar
-      width={1500}
       data={data}
-      keys={['degress']}
+      keys={['degrees']}
       indexBy='day'
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.4}
