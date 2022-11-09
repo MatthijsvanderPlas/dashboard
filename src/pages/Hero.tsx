2588d6f61f8b9f933d61176d67976b16649ca43d
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-11-09 22:37:03 +0100

    TS error fix

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,11 +1,11 @@
-import { useOutletContext } from 'react-router-dom';
+// import { useOutletContext } from 'react-router-dom';
 import BarChart from '~/components/BarChart';
-import { CsvData } from '~/App';
-import useStore, { Data } from '~/useStore';
+// import { CsvData } from '~/App';
+// import useStore, { Data } from '~/useStore';
 
 export default function Hero() {
-  const allStudentData: CsvData[] = useOutletContext();
-  const { data }: { data: Data[] } = useStore(allStudentData);
+  // const allStudentData: CsvData[] = useOutletContext();
+  // const { data }: { data: Data[] } = useStore(allStudentData);
 
   return (
     <>
